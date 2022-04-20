e0da86f7cb1a6267867139de020fea0a7805d062
Merijn van Aartrijk <merijn@poort8.nl>
2022-04-20 12:15:10 +0200

    Always show swagger UI.

--- Poort8.Ishare.Common/Program.cs
+++ Poort8.Ishare.Common/Program.cs
@@ -12,11 +12,8 @@ builder.Services.AddIshareCoreServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
